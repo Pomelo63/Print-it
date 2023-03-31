12ab7c01945efe98ef97bae2cbe71b281367038d
Pomelo63 <124713442+Pomelo63@users.noreply.github.com>
2023-03-31 18:02:59 +0200

    [MODIFY] style et script.js

--- assets/script.js
+++ assets/script.js
@@ -12,12 +12,12 @@ const slides = [
 	{
 		"image": "slide3.jpg",
 		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-		"alt" : "Etablissement d'une charte graphique"
+		"alt": "Etablissement d'une charte graphique"
 	},
 	{
 		"image": "slide4.png",
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>",
-        "alt" : "Autocollants de cartoons"
+		"alt": "Autocollants de cartoons"
 	}
 ]
 
@@ -26,18 +26,17 @@ let count = 0; //Var qui permet de savoir sur quel slide on est
 let dotCount = count + 1; //Var qui permert de savoir quel dot mettre en selected et unselected
 leftArrow = document.querySelector(".arrow_left"); //Const qui récupère la classe fleche gauche
 rightArrow = document.querySelector(".arrow_right"); //Const qui récupère la classe fleche droite
-bannerImg = document.querySelector(".banner-img");//Const qui récupère l'img de la ban
 
 //Création de la section dots dans l'id Banner
 const sectionBanner = document.querySelector("#banner");
 const dots = document.createElement('div');
+dots.className = "dots";
+sectionBanner.appendChild(dots);
 
 this.initialize();
 
 //Déclaration fonction d'initialisation
 function initialize() {
-	dots.className = "dots";
-	sectionBanner.appendChild(dots);
 	//Boucle for pour créer un dot à chaque image de la liste.
 	for (let i = 0; i < slides.length; i++) {
 		const dot = document.createElement('div');
@@ -45,7 +44,7 @@ function initialize() {
 		dots.appendChild(dot);
 	}
 	//Met le dot_selected sur le premier slide
-	document.querySelector(".dot:nth-of-type(1)").className = "dot dot_selected";
+	document.querySelector(".dot:nth-of-type(1)").classList.add("dot_selected");
 	//action au click sur le fleche droite
 	rightArrow.addEventListener("click", nextSlides);
 	//fonction au click sur la fleche gauche
@@ -54,7 +53,7 @@ function initialize() {
 
 //fonction pour passer au slide suivant
 function nextSlides() {
-	document.querySelector('.dot:nth-of-type(' + dotCount + ')').className = "dot";
+	document.querySelector('.dot:nth-of-type(' + dotCount + ')').classList.remove("dot_selected");
 	count++;
 	dotCount = count + 1;
 	//remise à 0 si compteur=slides.length
@@ -65,12 +64,12 @@ function nextSlides() {
 	document.querySelector(".banner-img").src = './assets/images/slideshow/' + slides[count].image;
 	document.querySelector(".banner-img").alt = slides[count].alt;
 	document.querySelector("p").innerHTML = slides[count].tagLine;
-	document.querySelector('.dot:nth-of-type(' + dotCount + ')').className = "dot_selected dot";
+	document.querySelector('.dot:nth-of-type(' + dotCount + ')').classList.add("dot_selected");
 }
 
 //fonction pour passer au slide précédent
 function backSlides() {
-	document.querySelector('.dot:nth-of-type(' + dotCount + ')').className = "dot";
+	document.querySelector('.dot:nth-of-type(' + dotCount + ')').classList.remove("dot_selected");
 	count--;
 	dotCount = count + 1;
 	//remise à slides.length si compteur<0
@@ -81,7 +80,7 @@ function backSlides() {
 	document.querySelector(".banner-img").src = './assets/images/slideshow/' + slides[count].image;
 	document.querySelector(".banner-img").alt = slides[count].alt;
 	document.querySelector("p").innerHTML = slides[count].tagLine;
-	document.querySelector('.dot:nth-of-type(' + dotCount + ')').className = "dot_selected dot";
+	document.querySelector('.dot:nth-of-type(' + dotCount + ')').classList.add("dot_selected");
 }
 
 
